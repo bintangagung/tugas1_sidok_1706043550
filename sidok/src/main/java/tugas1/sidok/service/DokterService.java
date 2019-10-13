61efbcc25581dc580a006819fb09c2cc554896bf
bintang agung nusantara <bintangagung1505@gmail.com>
2019-10-13 22:35:52 +0700

    add:dokterService

--- sidok/src/main/java/tugas1/sidok/service/DokterService.java
+++ sidok/src/main/java/tugas1/sidok/service/DokterService.java
@@ -1,4 +1,25 @@
 package tugas1.sidok.service;
 
+import tugas1.sidok.model.DokterModel;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface DokterService {
+    // Method untuk menambah Dokter
+    void addDokter(DokterModel dokter);
+
+    // Method untuk mendapatkan semua data Dokter yang tersimpan
+    List<DokterModel> getDokterList();
+
+    // Method untuk mendapatkan data sebuah Dokter berdasarkan idDokter
+    Optional<DokterModel> getDokterByIdDokter(Long idDokter);
+
+    DokterModel changeDokter(DokterModel dokterModel);
+
+    //viewAll
+    List<DokterModel> getDokterListOrderByNama();
+
+    //method delete restoran
+    void deleteDokter(DokterModel Dokter);
 }
--- sidok/src/main/java/tugas1/sidok/service/DokterServiceImpl.java
+++ sidok/src/main/java/tugas1/sidok/service/DokterServiceImpl.java
@@ -1,4 +1,47 @@
 package tugas1.sidok.service;
 
-public class DokterServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import tugas1.sidok.model.DokterModel;
+import tugas1.sidok.repository.DokterDb;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DokterServiceImpl implements DokterService {
+    @Autowired
+    private DokterDb dokterDb;
+
+    @Override
+    public void addDokter(DokterModel dokter) { dokterDb.save(dokter); }
+
+    @Override
+    public List<DokterModel> getDokterList() { return dokterDb.findAll(); }
+
+    @Override
+    public Optional<DokterModel> getDokterByIdDokter(Long idDokter) {
+        return dokterDb.findByIdDokter(idDokter);
+    }
+
+    @Override
+    public DokterModel changeDokter(DokterModel dokterModel) {
+        DokterModel targetDokter = dokterDb.findById(dokterModel.getIdDokter()).get();
+        try {
+            targetDokter.setNama(dokterModel.getNama());
+            targetDokter.setJenisKelamin(dokterModel.getJenisKelamin());
+            targetDokter.setTanggalLahir(dokterModel.getTanggalLahir());
+            targetDokter.setTempatLahir(dokterModel.getTempatLahir());
+            dokterDb.save(targetDokter);
+            return targetDokter;
+        } catch (NullPointerException nullException) {
+            return null;
+        }
+    }
+
+    @Override
+    public List<DokterModel> getDokterListOrderByNama() {
+        return dokterDb.findAllByOrderByNamaAsc();
+    }
+
+    @Override
+    public void deleteDokter(DokterModel dokter) { dokterDb.delete(dokter); }
 }
