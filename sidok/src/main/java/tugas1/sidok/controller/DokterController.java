13e2ce449ba0c7e80aba2373ab23a76bf454e286
bintang agung nusantara <bintangagung1505@gmail.com>
2019-10-25 17:36:57 +0700

    add:viewAllDokter

--- sidok/src/main/java/tugas1/sidok/controller/DokterController.java
+++ sidok/src/main/java/tugas1/sidok/controller/DokterController.java
@@ -56,9 +56,12 @@ public class DokterController {
         char generatedRandom = chars.charAt(rnd.nextInt(2));
         int tahunLahir = LocalDateTime.now().getYear() + 5 ;
         String tanggalLahir = String.valueOf(dokter.getTanggalLahir().getDate()) + String.valueOf(dokter.getTanggalLahir().getMonth()) + String.valueOf(dokter.getTanggalLahir().getYear());
+
         String nip = String.valueOf(tahunLahir) + tanggalLahir + dokter.getJenisKelamin() + generatedRandom;
         dokter.setNip(nip);
 
+        if (dokter.getJenisKelamin().equals("1")) dokter.setJenisKelamin("Laki-Laki");
+        if (dokter.getJenisKelamin().equals("2")) dokter.setJenisKelamin("Perempuan");
         dokterService.addDokter(dokter);
 
         System.out.println ("berhasil add dokter");
